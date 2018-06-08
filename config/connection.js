547884a95c2900bf25548cc7db45457c87481b69
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-07 19:28:53 -0700

    Converted

--- config/connection.js
+++ config/connection.js
@@ -1,44 +1,41 @@
 
-const mysql = require('mysql2');
-const config = require('./connection-config');
+var mysql = require('mysql2');
+var config = require('./connection-config');
 
+var connection = mysql.createConnection(config);
 
-class Connection {
-  constructor(config) {
-    this.connection = mysql.createConnection(config);
-  }
+//********** IN CASE I GET THE GREEN LIGHT FOR PROMISES ******************/
 
-  query(str,obj) {
-    return new Promise((resolve,reject) => {
-      this.connection.query(str,obj,(err,res) => {
-        if (err) {
-          throw err;
-          return reject(err);
-        }
-        resolve(res);
-      });
-    });
-  }
+//********** CUSTOM PROMISE WRAPPER ******************/
+// class Connection {
+//   constructor(config) {
+//     this.connection = mysql.createConnection(config);
+//   }
 
-  destroy() {
-    return new Promise((resolve,reject) => {
-      this.connection.destroy((err,res) => {
-        if(err) {
-          return reject(err);
-        }
-        resolve();
-      });
-    });
-  }
+//   query(str,obj) {
+//     return new Promise((resolve,reject) => {
+//       this.connection.query(str,obj,(err,res) => {
+//         if (err) {
+//           throw err;
+//           return reject(err);
+//         }
+//         resolve(res);
+//       });
+//     });
+//   }
 
-}
+//   destroy() {
+//     return new Promise((resolve,reject) => {
+//       this.connection.destroy((err,res) => {
+//         if(err) {
+//           return reject(err);
+//         }
+//         resolve();
+//       });
+//     });
+//   }
 
-module.exports = new Connection(config);
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'chickens_db'
-// });
+// }
 
-// module.exports = connection;
+// module.exports = new Connection(config);
+module.exports = connection;
