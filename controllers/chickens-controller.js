3f8c9433664438873089aededae8e132763d009a
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-07 19:22:48 -0700

    Remove await try/catch (converted)

--- controllers/chickens-controller.js
+++ controllers/chickens-controller.js
@@ -1,20 +1,11 @@
 
-const express = require('express');
-const chicken = require('../models/chicken');
+var express = require('express');
+var chicken = require('../models/chicken');
 
 // Set up router
-const router = express.Router();
+var router = express.Router();
 
 //  Routes
-router.get('/', async (req, res) => {
-  let allChickens;
-
-  try {
-    allChickens = await chicken.selectAll();
-    res.status(200).send(allChickens.json);
-  } catch (err) {
-    console.error(err);
-    res.status(404).end();
-  }
-
+router.get('/', function(req, res) {
+  
 });
