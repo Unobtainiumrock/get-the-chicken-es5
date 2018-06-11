56a3d13e37859f5af35b6c4ed246043fa39bca02
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-11 00:56:29 -0700

    jaws stuff

--- config/connection-config.js
+++ config/connection-config.js
@@ -1,10 +1,12 @@
 
 // Config to be passed to the instance of MySQL's connection in connections.js
 const config = {
-  host: process.env.HOST || 'localhost',
+  host: process.env.JAWSDB_URL || 'localhost',
   user: 'root',
   password: '',
   database: 'chickens_db'
 };
+
+var blah;
 
 module.exports = config;
