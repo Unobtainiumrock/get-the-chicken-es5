f19801f2aa5184054e6001850bff9d2e28618a3c
Unobtainiumrock <unobtainiumrock@gmail.com>
2018-06-11 10:04:14 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,18 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection(config);
+connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "",
+      database: "chickens_db"
+    });
 }
 
 
 
-//********** IN CASE I GET THE GREEN LIGHT FOR PROMISES ******************/
+//********** IN CASE  GET THE GREEN LIGHT FOR PROMISES ******************/
 
 //********** CUSTOM PROMISE WRAPPER ******************/
 // class Connection {
@@ -50,17 +56,4 @@ if (process.env.JAWSDB_URL) {
 // var connection;
 // var JAWSDB_URL = "mysql://r64k0qq5l871t1c4:t4lvasrez89f0c9w@qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nm8ychixahn6dspd";
 
-
-
-// if ( process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {``
-// connection = mysql.createConnection({
-//         host: "localhost",
-//         port: 3306,
-//         user: "root",
-//         password: "",
-//         database: "chickens_db"
-//       });
-// }
 module.exports = connection;
